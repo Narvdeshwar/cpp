b26ba1b8f5f54cfd3b9a147f2782a893b29ed631
eternal02.oct <eternal02.oct@gmail.com>
2025-02-08 07:02:37 +0000

    first cpp program

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,42 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.docker
-    pkgs.docker-compose
-  ];
-  # Enable Docker
-  services.docker = {
-    enable = true;
-  };
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "vscjava.vscode-java-pack"
-      "rangav.vscode-thunder-client"
-    ];
-  };
-}
+
+      # Which nixpkgs channel to use.
+      channel = "stable-23.11"; # or "unstable"
+
+      # Use https://search.nixos.org/packages to find packages
+      packages = [
+        pkgs.gcc
+      ];
+
+      # Sets environment variables in the workspace
+      env = {
+        SOME_ENV_VAR = "hello";
+      };
+
+      # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+      idx.extensions = [
+        "angular.ng-template"
+      ];
+
+      # Enable previews and customize configuration
+      idx.previews = {
+        enable = true;
+        previews = {
+          web = {
+            command = [
+              "npm"
+              "run"
+              "start"
+              "--"
+              "--port"
+              "$PORT"
+              "--host"
+              "0.0.0.0"
+              "--disable-host-check"
+            ];
+            manager = "web";
+          };
+        };
+      };
+    }
