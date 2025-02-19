3d29ccbfa1fd11cca8f135fed4b00f01a1f1fbae
narvdeshwar <eternal02.oct@gmail.com>
2025-02-19 23:14:36 +0530

    Optimise : shifting of array

--- Day07/shiftArray.cpp
+++ Day07/shiftArray.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> shiftArray(vector<int> &arr, int size, int shift)
+void shiftArray(vector<int> &arr, int size, int shift)
 {
+    // This approach uses the O(n) time and O(K) as a space complexity.
     int actualShift = shift % size;
     // base condition if actual shift is multiple of size then we don't need to perform the shifting operation
     if (actualShift == 0)
-        return arr;
+        return;
 
-    // store the shifting variable into the temp array
+    // store the data into the temp array which we have to shift
     vector<int> temp;
 
     for (int i = size - actualShift; i < size; i++)
     {
         temp.push_back(arr[i]);
     }
+
     // shifting of array element
     for (int i = size - actualShift - 1; i >= 0; i--)
     {
@@ -24,15 +26,35 @@ vector<int> shiftArray(vector<int> &arr, int size, int shift)
     {
         arr[i] = temp[i];
     }
-    for (auto &elem : arr)
+}
+void revereArray(vector<int> &arr, int startIndex, int endIndex)
+{
+    while (startIndex < endIndex)
     {
-        cout << elem << endl;
+        swap(arr[startIndex++], arr[endIndex--]);
     }
 }
+
+void shiftArrayOptimised(vector<int> &arr, int size, int shift)
+{
+    // calculate the actual count of shift first
+    int actualShift = shift % size;
+
+    // step 1: reverse the whole array like our array is 10,20,30,40,50
+    revereArray(arr, 0, size - 1);
+    // now the after performing step 1 array become 50,40,30,20,10
+
+    // step 2: reverse the array 
+}
+
 int main()
 {
     vector<int> arr = {10, 20, 30, 40, 50};
     int shift = 4;
     shiftArray(arr, arr.size(), shift);
+    for (auto &elem : arr)
+    {
+        cout << elem << " ";
+    }
     return 0;
 }
