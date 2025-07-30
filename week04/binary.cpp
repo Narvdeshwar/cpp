c2b9c746b7e590907693b6376fe84e47d2763aaa
narvdeshwar <sam.07soni@gmail.com>
2025-07-30 06:54:50 +0530

    binary search

--- week04/binary.cpp
+++ week04/binary.cpp
@@ -3,11 +3,24 @@ using namespace std;
 int binarySearch(vector<int> nums, int target)
 {
   int start = 0, end = nums.size() - 1;
-  while (start <= end)
+  while (start < end)
   {
-    int mid = start - (end - start) / 2;
-    
+    int mid = (end + start) / 2;
+    cout << mid << endl;
+    if (nums[mid] == target)
+    {
+      return mid;
+    }
+    else if (nums[mid] < target)
+    {
+      end = mid - 1;
+    }
+    else
+    {
+      start = mid + 1;
+    }
   }
+  return -1;
 }
 int main()
 {
